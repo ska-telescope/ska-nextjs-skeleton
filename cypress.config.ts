4e171cfea0e5d05939ab67b69909df339e4c2802
gallacherc <chloe.gallacher@cgi.com>
2023-11-08 21:06:18 +0000

    STAR-80: Refactored home e2e tests and updated format to be as standard

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: "http://localhost:3000",
-    specPattern: "./cypress/e2e/*.e2e.{js,jsx,ts,tsx}",
   },
   component: {
     devServer: {
---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,48 @@
+context("Home Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it('Header : Verify external link to skao site', () => {
+    cy.get('[data-testid="skaoLogo"]').click();
+  });
+
+  it('Header : Verify light/dark mode is available', () => {
+    cy.get('[data-testid="Brightness7Icon"]').click();
+    cy.get('[data-testid="Brightness4Icon"]').should('be.visible');
+    cy.get('[data-testid="Brightness4Icon"]').click();
+    cy.get('[data-testid="Brightness7Icon"]').should('be.visible');
+  });
+
+  it('Footer : Verify Title and Telescope selector', () => {
+    cy.get('[data-testid="footerId"]').contains('Copyright 2023 | DRAFT').should('be.visible');
+  });
+
+  it("should have a title as NextJS Skeleton", () => {
+    cy.title().should("contains", "NextJS Skeleton");
+  });
+
+  it("Render: Should have content detailing usage", () => {
+    cy.get('[data-testid="titleId"]').contains('Welcome').should('be.visible');
+    cy.get('[data-testid="text1Id"]')
+        .contains('simple implementation of a NextJS Application').should('be.visible');
+    cy.get('[data-testid="text2Id"]')
+        .contains('used as a basis for new applications for the SKAO').should('be.visible');
+    cy.get('[data-testid="text3Id"]')
+        .contains('MUI grid which can be used for component layout').should('be.visible');
+    cy.get('[data-testid="text4Id"]')
+        .contains('see the documentation available at this link').should('be.visible');
+    cy.get('[data-testid="text5Id"]')
+        .contains('https://developer.skao.int/projects/ska-gui-components/en/latest/?badge=latest')
+        .should('be.visible');
+  });
+
+  it('Render : Verify continue button and page', () => {
+    cy.get('[data-testid="continueButton"]').click();
+    cy.get('[data-testid="textId"]').should('be.visible');
+    cy.get('[data-testid="numberId"]').should('be.visible');
+    cy.get('[data-testid="testId"]').should('be.visible');
+    cy.get('[data-testid="statusId"]').should('be.visible');
+    cy.get('[data-testid="continueButton"]').should('be.visible');
+  });
+});
