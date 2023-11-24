bded7da4712546b486b7c54600f8c82531f7bc73
t.swain <trevor.swain@cgi.com>
2023-11-24 16:35:23 +0000

    STAR-94 : Addition of Observation page

--- src/app/proposal/observation/page.tsx
+++ src/app/proposal/observation/page.tsx
@@ -1,11 +1,35 @@
 'use client';
 
 import React from 'react';
+import { DataContext } from '@/components/layout/dataProvider/DataProvider.jsx';
 import PageBanner from '@/components/layout/pageBanner/PageBanner';
 import PageFooter from '@/components/layout/pageFooter/PageFooter';
-import { Grid, Typography } from '@mui/material';
+import DataGridWrapper from '@/components/wrappers/dataGridWrapper/dataGridWrapper';
+import SensCalcButton from '@/components/button/sensCalc/SensCalcButton';
+import { Card, CardContent, CardHeader, Checkbox, FormControlLabel, Grid, Typography } from '@mui/material';
+import useTheme from '@mui/material/styles/useTheme';
 
 export default function Observation() {
+  const { data } = React.useContext(DataContext);
+  const theme = useTheme();
+
+  const ClickFunction = () => {
+  };
+
+  const columnsObservations = [
+    { field: 'id', headerName: 'Array', width: 100 },
+    { field: 'title', headerName: 'Subarray', width: 200 },
+    { field: 'pi', headerName: 'Linked Targets', width: 200 },
+    { field: 'status', headerName: 'Observation Type', width: 100 }
+  ];
+  const extendedColumnsObservations = structuredClone(columnsObservations);
+
+  const columnsTargets = [
+    { field: 'Name', headerName: 'Name', width: 200 },
+    { field: 'RA', headerName: 'Right Ascension ( hh:mm:ss:s )', width: 250 },
+    { field: 'Dec', headerName: 'Declination ( dd:mm:ss:s )', width: 250 }
+  ];
+  const extendedColumnsTargets = structuredClone(columnsTargets);
 
   return (
     <Grid container direction="column" alignItems="space-evenly" justifyContent="space-around" >
@@ -13,9 +37,62 @@ export default function Observation() {
         <PageBanner title="Observation"/>
       </Grid>
       <Grid item>
-        <Typography variant="h6" m={2}>
-          DETAILS NEEDED : ??????
-        </Typography>
+        <Grid p={1} container direction="row" alignItems="space-evenly" justifyContent="space-around" >
+          <Grid item xs={5}>
+            <Grid container direction="column" alignItems="center" justifyContent="space-around">
+              <DataGridWrapper rows={data.ExistingProposal} extendedColumns={extendedColumnsObservations} height={500} rowClick={ClickFunction} />
+            </Grid>
+          </Grid>
+          <Grid item xs={7}>
+            <Card variant='outlined'>
+              <CardHeader title={<Typography variant='body2'>Target List related to the selected Observation</Typography>} />
+              <CardContent>
+                <Grid container direction="row" alignItems="center" justifyContent="space-around">
+                  <Grid item xs={3}>
+                    <Grid container direction="column" alignItems="flex-start" justifyContent="space-between" >
+
+                      <FormControlLabel
+                        value="linked"
+                        control={
+                          <Checkbox
+                            defaultChecked
+                            sx={{
+                              '&.Mui-checked': {
+                                color: theme.palette.secondary.main,
+                              },
+                            }}
+                          />}
+                        label="Linked"
+                        labelPlacement="end"
+                        sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }}
+                      />
+                      <FormControlLabel
+                        value="unlinked"
+                        control={
+                          <Checkbox
+                            sx={{
+                              '&.Mui-checked': {
+                                color: theme.palette.secondary.main,
+                              },
+                            }}
+                          />}
+                        label="Unlinked"
+                        labelPlacement="end"
+                        sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }}
+                      />
+
+                      <SensCalcButton />
+                    </Grid>
+                  </Grid>
+                  <Grid item >
+                    <DataGridWrapper rows={data.Targets.ListOfTargets.TargetItems} extendedColumns={extendedColumnsTargets} height={407} rowClick={ClickFunction} />
+                  </Grid>
+                </Grid>
+              </CardContent>
+
+            </Card>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item>
         <PageFooter previousPageLabel='target' previousPageURL='/proposal/target' nextPageLabel='technical' nextPageURL='/proposal/technical' />
@@ -23,4 +100,3 @@ export default function Observation() {
     </Grid>
   );
 }
-
---
+++ src/components/button/sensCalc/SensCalcButton.tsx
@@ -0,0 +1,25 @@
+'useClient';
+
+import React from 'react';
+import { Button, ButtonColorTypes, ButtonVariantTypes } from '@ska-telescope/ska-gui-components';
+import CalculateIcon from '@mui/icons-material/Calculate';
+
+export default function SensCalcButton() {
+  const ClickFunction = () => {
+    // location.assign('/');
+  };
+
+  const title = 'Sensitivity Calculator';
+
+  return (
+    <Button
+      ariaDescription={title + 'Button'}
+      color={ButtonColorTypes.Inherit}
+      icon={<CalculateIcon/>}
+      label={title}
+      onClick={ClickFunction}
+      testId={title + 'Button'}
+      variant={ButtonVariantTypes.Contained}
+    />
+  );
+}
