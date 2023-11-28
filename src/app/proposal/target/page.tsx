e3a302f9546850c39bca3f9ff6562aae80ddbc21
Sarah Sattar <sarah.sattar@criticalsoftware.com>
2023-11-28 17:20:09 +0000

    STAR-94 list of targets view (in progress)

--- src/app/proposal/target/page.tsx
+++ src/app/proposal/target/page.tsx
@@ -29,76 +29,72 @@ export default function Target() {
   };
 
   return (
-    <Grid container direction="column" alignItems="space-evenly" justifyContent="space-around" >
+    <Grid container direction="column" justifyContent="space-around" >
       <Grid item>
         <PageBanner title="Target" />
       </Grid>
-      <Grid item>
-        <Typography variant="h6" m={2}>
-          DETAILS NEEDED : SARAH
-        </Typography>
-        <Grid container direction="row" alignItems="space-evenly" justifyContent="center" spacing={4}>
+      <Grid container direction="row" alignItems="space-evenly" justifyContent="center" spacing={4}>
+        <Grid item>
           <Grid item>
-            <Grid item>
-              <Card sx={{ width: 275, height: 140 }} className={`${styles.card} ${selectedCards[0].isSelected && styles.selected}`}>
-                <CardActionArea onClick={() => handleClick(0)} sx={{ minHeight: 140 }}>
-                  <CardContent>
-                    <Typography variant="h5" color={() => selectedCards[0].isSelected ? 'white' : 'text.secondary'} align="center" gutterBottom>
-                      No specific Target
-                    </Typography>
-                    <Typography variant="subtitle1" color={() => selectedCards[0].isSelected ? 'white' : 'text.secondary'} align="center">
-                      We&apos;re just going to look up
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Card sx={{ width: 275, height: 140 }} className={`${styles.card} ${selectedCards[1].isSelected && styles.selected}`}>
-              <CardActionArea onClick={() => handleClick(1)} sx={{ minHeight: 140 }}>
+            <Card sx={{ width: 275, height: 140 }} className={`${styles.card} ${selectedCards[0].isSelected && styles.selected}`}>
+              <CardActionArea onClick={() => handleClick(0)} sx={{ minHeight: 140 }}>
                 <CardContent>
-                  <Typography variant="h5" color={() => selectedCards[1].isSelected ? 'white' : 'text.secondary'} align="center" gutterBottom>
-                    List of Targets
+                  <Typography variant="h5" color={() => selectedCards[0].isSelected ? 'white' : 'text.secondary'} align="center" gutterBottom>
+                    No specific Target
                   </Typography>
-                  <Typography variant="subtitle1" color={() => selectedCards[1].isSelected ? 'white' : 'text.secondary'} align="center">
-                    A list of target will be entered and/or imported from file
+                  <Typography variant="subtitle1" color={() => selectedCards[0].isSelected ? 'white' : 'text.secondary'} align="center">
+                    We&apos;re just going to look up
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
           </Grid>
-          <Grid item>
-            <Grid item>
-              <Card sx={{ width: 275, height: 140 }} className={`${styles.card} ${selectedCards[2].isSelected && styles.selected}`}>
-                <CardActionArea onClick={() => handleClick(2)} sx={{ minHeight: 140 }}>
-                  <CardContent>
-                    <Typography variant="h5" color={() => selectedCards[2].isSelected ? 'white' : 'text.secondary'} align="center" gutterBottom>
-                      Target Mosaic
-                    </Typography>
-                    <Typography variant="subtitle1" color={() => selectedCards[2].isSelected ? 'white' : 'text.secondary'} align="center">
-                      Using a tool to create a mosaic of targets
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid className={`${styles.contentContainer}`} container direction="column" justifyContent="flex-end" alignItems="center">
-          <Grid item>
-            {selectedCards[0].isSelected && <TargetNoSpecificSection />}
-          </Grid>
-          <Grid item>
-            {selectedCards[1].isSelected && <TargetListSection />}
-          </Grid>
-          <Grid item>
-            {selectedCards[2].isSelected && <TargetMosaicSection />}
-          </Grid>
         </Grid>
         <Grid item>
-          <PageFooter previousPageLabel='science' previousPageURL='/proposal/science' nextPageLabel='observation' nextPageURL='/proposal/observation' />
+          <Card sx={{ width: 275, height: 140 }} className={`${styles.card} ${selectedCards[1].isSelected && styles.selected}`}>
+            <CardActionArea onClick={() => handleClick(1)} sx={{ minHeight: 140 }}>
+              <CardContent>
+                <Typography variant="h5" color={() => selectedCards[1].isSelected ? 'white' : 'text.secondary'} align="center" gutterBottom>
+                  List of Targets
+                </Typography>
+                <Typography variant="subtitle1" color={() => selectedCards[1].isSelected ? 'white' : 'text.secondary'} align="center">
+                  A list of target will be entered and/or imported from file
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Grid>
+        <Grid item>
+          <Grid item>
+            <Card sx={{ width: 275, height: 140 }} className={`${styles.card} ${selectedCards[2].isSelected && styles.selected}`}>
+              <CardActionArea onClick={() => handleClick(2)} sx={{ minHeight: 140 }}>
+                <CardContent>
+                  <Typography variant="h5" color={() => selectedCards[2].isSelected ? 'white' : 'text.secondary'} align="center" gutterBottom>
+                    Target Mosaic
+                  </Typography>
+                  <Typography variant="subtitle1" color={() => selectedCards[2].isSelected ? 'white' : 'text.secondary'} align="center">
+                    Using a tool to create a mosaic of targets
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid className={`${styles.contentContainer}`} container direction="column" justifyContent="space-between" alignItems="center">
+        <Grid item>
+          {selectedCards[0].isSelected && <TargetNoSpecificSection />}
+        </Grid>
+        <Grid item sx={{width: '100%'}}>
+          {selectedCards[1].isSelected && <TargetListSection />}
+        </Grid>
+        <Grid item>
+          {selectedCards[2].isSelected && <TargetMosaicSection />}
+        </Grid>
+      </Grid>
+      <Grid item>
+        <PageFooter previousPageLabel='science' previousPageURL='/proposal/science' nextPageLabel='observation' nextPageURL='/proposal/observation' />
       </Grid>
     </Grid >
   );
